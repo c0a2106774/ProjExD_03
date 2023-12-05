08fcb34b0f7b83e8928d05dff66531754d2e0c6b
c0a2106774 <c0a2106774@edu.teu.ac.jp>
2023-12-05 14:57:36 +0900

    練習3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -34,26 +34,27 @@ class Bird:
         引数1 num：こうかとん画像ファイル名の番号
         引数2 xy：こうかとん画像の位置座標タプル
         """
-        # img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 0, 2.0)
-        # img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
-        # self.imgs = {  # 0度から反時計回りに定義
-        #     (+5, 0): img,  # 右
-        #     (+5, -5): pg.transform.rotozoom(img, 45, 1.0),  # 右上
-        #     (0, -5): pg.transform.rotozoom(img, 90, 1.0),  # 上
-        #     (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),  # 左上
-        #     (-5, 0): img0,  # 左
-        #     (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
-        #     (0, +5): pg.transform.rotozoom(img, -90, 1.0),  # 下
-        #     (+5, +5): pg.transform.rotozoom(img, -45, 1.0),  # 右下
-        # }
-        self.img = pg.transform.flip(  # 左右反転
-            pg.transform.rotozoom(  # 2倍に拡大
-                pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 
-                0, 
-                2.0), 
-            True, 
-            False
-        )
+        img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 0, 2.0)
+        img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
+        self.imgs = {  # 0度から反時計回りに定義
+             (+5, 0): img,  # 右
+             (+5, -5): pg.transform.rotozoom(img, 45, 1.0),  # 右上
+             (0, -5): pg.transform.rotozoom(img, 90, 1.0),  # 上
+             (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),  # 左上
+             (-5, 0): img0,  # 左
+             (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
+             (0, +5): pg.transform.rotozoom(img, -90, 1.0),  # 下
+            (+5, +5): pg.transform.rotozoom(img, -45, 1.0),  # 右下
+            }
+        #self.img = pg.transform.flip(  # 左右反転
+            #pg.transform.rotozoom(  # 2倍に拡大
+            #pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 
+            #0, 
+            #2.0), 
+            #True, 
+            #False
+        #)
+        self.img = self.imgs[(+5,0)]
         self.rct = self.img.get_rect()
         self.rct.center = xy
     def change_img(self, num: int, screen: pg.Surface):
@@ -78,6 +79,8 @@ class Bird:
         self.rct.move_ip(sum_mv)
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.img = self.imgs[tuple(sum_mv)]
         screen.blit(self.img, self.rct)
 class Bomb:
     """
@@ -145,7 +148,7 @@ def main():
         if bomb is not None:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                bird.change_img(6, screen)
+                bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
                 return
@@ -154,6 +157,7 @@ def main():
             if beam.rct.colliderect(bomb.rct):
                 beam = None
                 bomb = None
+                bird.change_img(6,screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
